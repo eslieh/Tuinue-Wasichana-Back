778b297d90194a4a73981a144a1d55f8f78ec77a
craigkibera <craigituro@gmail.com>
2025-04-28 20:15:22 +0300

    cors

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_cors import CORS
 from models import db, bcrypt
 
 def create_app():
     app = Flask(__name__)
     
+    # allow only your Vite dev server on port 5173
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tuinue_wasichana.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = 'super-secret-key'  # Update with secure key in production
