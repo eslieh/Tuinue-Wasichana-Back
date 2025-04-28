08216c20b33c9eeb66c445b4cb72882bf6448b34
craigkibera <craigituro@gmail.com>
2025-04-28 23:05:07 +0300

    authentication retrive

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
 from models import db, bcrypt
+from dotenv import load_dotenv
+from utils import redis_client  
+from routes.authentication import auth_bp  
+
+# Load environment variables
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
@@ -9,13 +15,18 @@ def create_app():
     # allow only your Vite dev server on port 5173
     CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
+    # App configurations
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tuinue_wasichana.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.secret_key = 'super-secret-key'  # Update with secure key in production
+    app.secret_key = 'super-secret-key'
 
+    # Initialize extensions
     db.init_app(app)
     bcrypt.init_app(app)
     Migrate(app, db)
+
+    # Register blueprints
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
 
@@ -24,3 +35,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Database tables created.")
+    app.run(debug=True)
