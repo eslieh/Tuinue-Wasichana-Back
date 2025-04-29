f88db65c72f96d37f48d280d3daa823c4d0f5a8f
craigkibera <craigituro@gmail.com>
2025-04-29 14:53:52 +0300

    utils update

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
+import collections
+import collections.abc
+collections.Mapping = collections.abc.Mapping
+
+
+import jwt.algorithms
+jwt.algorithms.requires_cryptography = []
+
+import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db, bcrypt
 from dotenv import load_dotenv
-from utils import redis_client  
-from routes.authentication import auth_bp  
+from flask_jwt_extended import JWTManager
 
-# Load environment variables
+from models import db, bcrypt
+from utils import redis_client
+from routes.authentication import auth_bp
+from routes.charity import charity_bp
+
+# Load .env variables
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    
-    # App configurations
-    # allow only your Vite dev server on port 5173
     CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tuinue_wasichana.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.secret_key = 'super-secret-key'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'super-secret-key')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', app.config['SECRET_KEY'])
 
-    # Initialize extensions
     db.init_app(app)
     bcrypt.init_app(app)
     Migrate(app, db)
+    JWTManager(app)
 
-    # Register blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')
-
+    app.register_blueprint(charity_bp, url_prefix='/charities')
     return app
 
 if __name__ == '__main__':
