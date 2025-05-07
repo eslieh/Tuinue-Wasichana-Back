cc5feb6296349ec7769a3a282f114dfba5f6d1d0
eddy <eddy.mwaniki.27@gmail.com>
2025-05-07 23:36:18 +0300

    Edits gunicorn

--- app.py
+++ app.py
@@ -54,8 +54,9 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     with app.app_context():
         db.create_all()
         print("Database tables created.")
